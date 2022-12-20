a171960ba01ae3b2d6bea2f18eabb1e21737939a
Jonathan <jon.chua51@gmail.com>
2022-12-19 20:18:58 -0800

    added logging

--- routes/v1/v1.js
+++ routes/v1/v1.js
@@ -10,18 +10,21 @@ router.use('/movie', validPref);
 
 router.get('/movie', async (req, res) => {
     const result = await requestMovie(req.body);
+    console.log(req.body);
     if(result instanceof Error) { res.status(404).send(result.message); }
     else { res.status(201).send(result); } 
 });
 
 router.post('/register', async (req, res) => {
     const result = await register(req.body);
+    console.log(req.body);
     if(result instanceof Error) { res.status(403).send("account already taken"); }
     else { res.status(200).json(result); }
 });
 
 router.post('/account', async (req, res) => {
     const result = await authenticate(req.body);
+    console.log(req.body);
     if(result instanceof Error) { res.status(401).send("authentication process failed"); }
     else { res.status(201).json(result); }
 });
